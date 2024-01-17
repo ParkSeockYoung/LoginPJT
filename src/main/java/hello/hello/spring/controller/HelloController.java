5b151e10ac9333dd1c4bc9ed70e2acb8a5622d43
박석영 <142307816+ParkSeockYoung@users.noreply.github.com>
2024-01-17 11:28:59 +0900

    commit source code
    지금까지 작업한 코드 첫 commit

---
+++ src/main/java/hello/hello/spring/controller/HelloController.java
@@ -0,0 +1,37 @@
+package hello.hello.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    ModelAndView mav = new ModelAndView();
+
+    @GetMapping("/hello")
+    public ModelAndView hello() {
+        mav.setViewName("hello");
+        mav.addObject("data", "hello!!!!!");
+        return mav;
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        mav.setViewName("login");
+        return mav;
+    }
+
+    @PostMapping("/login/try")
+    @ResponseBody
+    public ModelAndView loginTry(@RequestParam("ID") String id, @RequestParam("PW") String password) {
+        mav.setViewName("login");
+        System.out.println(id);
+        System.out.println(password);
+        return mav;
+    }
+
+}
