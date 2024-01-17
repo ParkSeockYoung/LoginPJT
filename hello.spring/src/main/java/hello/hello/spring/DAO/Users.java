b0b1716f1c159f11ee88e82edd3f04b7332ba071
박석영 <142307816+ParkSeockYoung@users.noreply.github.com>
2024-01-17 10:31:29 +0900

    01.17 code
    메인 페이지 / 로그인 페이지 / 로그인 시도 PostMapping 까지 완료

---
+++ hello.spring/src/main/java/hello/hello/spring/DAO/Users.java
@@ -0,0 +1,23 @@
+package hello.hello.spring.DAO;
+
+public class Users {
+    private String id;
+    private String password;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
